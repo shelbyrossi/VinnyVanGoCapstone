49c48fa73f6ccd19cd22a80f21d183f616a7a035
ShelbyRae Rossi <91293692+shelbyrossi@users.noreply.github.com>
2022-01-05 15:43:34 -0600

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,7 @@ export const MainPage = () => {
   return (
     <center>
     <div>
-      <img src="https://res.cloudinary.com/dyjahmaif/image/upload/v1639514315/IMG_0746_tptg9o.jpg"/></div><div>
+      <img src="https://res.cloudinary.com/dyjahmaif/image/upload/v1641418835/IMG_0746_dpsohs.jpg"/></div><div>
 
     <div className="address">
      12 East Main St Nashville, Tennessee
